7113491be077d964ba6752cc65e618c9dd7c7414
mitul-7494 <mitulnagani.04@gmail.com>
2023-12-31 02:56:48 +0530

    deployment road 14

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ async function main(){
 }
 server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader("Access-Control-Allow-Headers", "*");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader('Access-Control-Allow-Methods', '*');
+  // res.setHeader("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
